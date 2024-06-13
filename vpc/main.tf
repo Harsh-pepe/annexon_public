d963e20088797fd4e0a216842b7a245bb733d90c
remotlee <61445903+Harsh-pepe@users.noreply.github.com>
2024-06-13 10:58:17 +0530

    Create main.tf

---
+++ vpc/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "annexon_dev" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "annexon_dev_private" {
+  vpc_id            = aws_vpc.annexon_dev.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "my-private-subnet"
+  }
+}
+
+data "aws_availability_zones" "available" {}
