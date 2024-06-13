f4d962fcf409dcb9f19f92702c6fd365062d1031
remotlee <61445903+Harsh-pepe@users.noreply.github.com>
2024-06-13 11:02:38 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAXCCHXJOHC2ZXWQQK"
+  secret_key = "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"
+}
+
+module "vpc" {
+  source = "./vpc"
+
+  vpc_cidr           = var.vpc_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnet_id" {
+  value = module.vpc.private_subnet_id
+}
